6d58d93845802b2f94c31f34a35f57de43c3158e
mallikarjuna3110 <114568878+mallikarjuna3110@users.noreply.github.com>
2022-10-10 20:18:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+
+
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Klae,Spinach & Rocket Smoothie')
